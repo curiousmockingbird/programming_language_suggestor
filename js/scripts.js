860be0b2cff72dcf29191d1e7ddfc1d839b2f340
Harold Mesa <harold.mesa@icloud.com>
2022-08-12 12:14:23 -0600

    Add survey questions. WIP:Working on UI and Business Logic

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,10 @@ window.onload = function () {
         event.preventDefault();
 
         const userName = document.getElementById("userName").value;
+        const movies = document.querySelector("input[name='radio']:checked").value;
         console.log(userName);
+        console.log(movies);
+
 
     });
 }
