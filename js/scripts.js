26e99da93c6bafa6a7429ec84e7510618e11101d
Harold Mesa <harold.mesa@icloud.com>
2022-08-12 14:15:44 -0600

    WIP: Working on results after submission and in UI Logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,29 @@
 window.onload = function () {
     let form = document.querySelector("form");
 
-    form.addEventListener("submit", function (event) {
+    function displayResults(event) {
         event.preventDefault();
-
+        // get the user input values
         const firstName = document.getElementById("firstName").value;
         const lastName = document.getElementById("lastName").value;
         const userName = firstName + " " + lastName;
-
         const age = document.getElementById("userAge").value;
         const movies = document.querySelector("input[name='radio']:checked").value;
-
-        console.log(movies);
-
         const freeTime = document.getElementById("hobby").value;
         const animal = document.querySelector("input[name='animal']:checked").value;
         const scale = document.getElementById("range").value;
 
-        console.log(freeTime);
+        document.getElementById("firstName").value = null;
+        document.getElementById("lastName").value = null;
+        document.getElementById("userAge").value = null;
+        document.querySelector("input[name='radio']:checked").value = null;
+        document.getElementById("hobby").value = null;
+        document.querySelector("input[name='animal']:checked").value = null;
+        document.getElementById("range").value = null;
 
 
 
+    };
 
-
-    });
+    form.addEventListener("submit", displayResults);
 }
