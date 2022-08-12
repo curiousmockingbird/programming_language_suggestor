d3f84eab9e3b28d684d9ed3dc6df0eba209be49e
Harold Mesa <harold.mesa@icloud.com>
2022-08-12 12:57:04 -0600

    WIP: Working on results after the submission

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,23 @@ window.onload = function () {
     form.addEventListener("submit", function (event) {
         event.preventDefault();
 
-        const userName = document.getElementById("userName").value;
+        const firstName = document.getElementById("firstName").value;
+        const lastName = document.getElementById("lastName").value;
+        const userName = firstName + " " + lastName;
+
+        const age = document.getElementById("userAge").value;
         const movies = document.querySelector("input[name='radio']:checked").value;
-        console.log(userName);
+
         console.log(movies);
+
+        const freeTime = document.getElementById("hobby").value;
+        const animal = document.querySelector("input[name='animal']:checked").value;
+        const scale = document.getElementById("range").value;
+
+        console.log(freeTime);
+
+
+
 
 
     });
